57aa29222b7e6b74c092636b00a87183a05adacf
jixyxcsoqi <91357188+jixyxcsoqi@users.noreply.github.com>
2022-01-29 19:50:45 +0800

    Add files via upload

---
+++ 一.java
@@ -0,0 +1,9 @@
+
+public class 一{ // 这是一个类，类名叫做HelloWorld
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("动力节点-口口相传的Java黄埔军校");
+		char x='\u5e65';
+		System.out.print(x);
+	}
+}
